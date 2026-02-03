fe679da0bfb114c1b2f03a1e5b6a58c83c99e15e
soumen88 <dassoumen2488@gmail.com>
2026-02-03 10:37:46 +0530

    Before publishing to playstore

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -3,6 +3,8 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_expandable_fab/flutter_expandable_fab.dart';
 import 'package:tcsgoalnest/core/constants/color_constants.dart';
+import 'package:tcsgoalnest/core/dependency/injectable_setup.dart';
+import 'package:tcsgoalnest/core/table/goal_store_manager.dart';
 import 'package:tcsgoalnest/ui/commonwidgets/custom_app_bar.dart';
 import 'package:tcsgoalnest/ui/commonwidgets/custom_loader.dart';
 import 'package:tcsgoalnest/ui/commonwidgets/display_error_widget.dart';
@@ -11,29 +13,96 @@ import 'package:tcsgoalnest/ui/commonwidgets/home/top_bar_widget.dart';
 import 'package:tcsgoalnest/ui/commonwidgets/regular_text_widget.dart';
 
 import '../../core/routing/app_router.dart';
+import '../../core/schema/goal_store_data.dart';
 import '../commonwidgets/filled_button_widget.dart';
 
 @RoutePage()
 class HomeScreen extends StatelessWidget {
   final _key = GlobalKey<ExpandableFabState>();
-
+  final _goalManager = locator<GoalStoreManager>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            TopBarWidget(),
-            NoGoalsWidget(),
-            FilledButtonWidget(
-                buttonCaption: "Create A Goal",
-                onButtonPress: (){
-                  context.router.push(const GoalTrackerRoute());
-                },
-            )
-          ],
-        ),
+      body: Column(
+        children: [
+          TopBarWidget(),
+          Expanded(
+              child: StreamBuilder(
+                  stream: _goalManager.listenToGoals(),
+                  builder: (BuildContext context, AsyncSnapshot<List<GoalStoreData>> snapshot){
+                    if(snapshot.connectionState == ConnectionState.waiting){
+                      return SizedBox(
+                          height: 200,
+                          child: CustomLoader()
+                      );
+                    }
+                    else{
+                      if(snapshot.hasError){
+                        return DisplayErrorWidget(
+                          errorMessage: snapshot.error.toString(),
+                        );
+                      }
+                      else{
+                        List<GoalStoreData> goals = snapshot.data ?? [];
+                        Map<String, int> goalCountByType = {};
+                        Map<String, GoalStoreData> goalTypes = {};
+                        for(var currentGoal in goals){
+                          goalCountByType[currentGoal.goalType] = (goalCountByType[currentGoal.goalType] ?? 0) + 1;
+                          if(!goalTypes.containsKey(currentGoal.goalType)){
+                            goalTypes[currentGoal.goalType] = currentGoal;
+                          }
+                        }
+                        return Builder(
+                            builder: (BuildContext context){
+                              if(goals.isEmpty){
+                                return NoGoalsWidget();
+                              }
+                              else{
+                                return GridView.builder(
+                                    shrinkWrap: true,
+                                    physics: NeverScrollableScrollPhysics(),
+                                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                                    itemCount: goalTypes.length,
+                                    itemBuilder: (BuildContext context, int index){
+                                      GoalStoreData data = goalTypes.values.toList()[index];
+                                      int goalsFound = goalCountByType[data.goalType] ?? 0;
+                                      return Card(
+                                        child: Column(
+                                          children: [
+                                            CircleAvatar(
+                                              maxRadius: 70,
+                                              child: Image.asset(
+                                                data.goalAssetImage,
+                                                width: 150,
+                                                height: 150,
+                                              ),
+                                            ),
+                                            RegularTextWidget(
+                                                textToDisplay: "${data.goalType} (${goalsFound})"
+                                            )
+                                          ],
+                                        ),
+                                      );
+                                    }
+                                );
+
+                              }
+                            }
+                        );
+                      }
+                    }
+                  }
+              )
+          ),
+
+          FilledButtonWidget(
+            buttonCaption: "Create A Goal",
+            onButtonPress: (){
+              context.router.push(const GoalTrackerRoute());
+            },
+          )
+        ],
       ),
       floatingActionButtonLocation: ExpandableFab.location,
       floatingActionButton: ExpandableFab(
